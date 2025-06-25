aa5dd07b5add383368000aa1495e3c5b95130aa0
akashjadhav3348 <akashjadhav3348@gmail.com>
2025-06-25 11:00:02 +0530

    Change text

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Set up a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, Akash! Welcome to your Node.js application.');
   
 });
 
